606f5d30253c0f225ca5775231cca72e584b1388
Nadia Charonov <charonovnadia@gmail.com>
2021-11-23 19:20:25 -0500

    maj

---
+++ src/ift3325_tp2/Emetteur.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Emetteur {
+
+	public int polynome = 0b10001000000100001;
+	public List<String> data;
+	
+	public Emetteur(String fileDir) {
+		this.data = readFile(fileDir);
+	}
+	
+	public List<String> readFile(String fileDir){
+		List<String> data = new ArrayList<String>();
+		try {
+			File fileData = new File(fileDir);
+			Scanner myReader = new Scanner(fileData);
+			while (myReader.hasNextLine()) {
+				data.add(myReader.nextLine());
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+	
+		return data;
+		
+	}
+	
+	
+	
+}
